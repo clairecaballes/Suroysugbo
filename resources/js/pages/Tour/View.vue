32f807e7684135fbac2e01c7a152b6c6cf66a395
clairecaballes <jeanclairecaballes20000@gmail.com>
2025-06-25 11:51:49 -0700

    BACK BUTTON CEBU LEGACY

--- resources/js/pages/Tour/View.vue
+++ resources/js/pages/Tour/View.vue
@@ -1,5 +1,10 @@
 <template>
     <Head :title="`${props.legacyItem?.title}`" />
+    <div class="mb-4">
+  <Link href="/" class="inline-block px-4 py-2 bg-gray-200 text-gray-700 rounded hover:bg-gray-300">
+    ← Back to Home
+  </Link>
+</div>
     <div class="flex flex-col lg:flex-row gap-8 w-full max-w-6xl mx-auto p-4 lg:p-6 bg-white rounded-lg shadow-lg mt-4 lg:mt-7">
         <!-- Left: Image Container -->
         <div class="flex flex-col items-center lg:items-start">
@@ -73,6 +78,7 @@
 import { ref, onMounted, onUnmounted, inject, nextTick } from 'vue'
 import { GoogleMap, AdvancedMarker } from 'vue3-google-map'
 import { Head } from '@inertiajs/vue3'
+import { Link } from '@inertiajs/vue3'
 
 
 const container = ref(null)
