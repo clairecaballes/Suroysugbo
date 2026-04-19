00092ba6d9c95b1427a88b0a57872db294f53d02
clairecaballes <jeanclairecaballes20000@gmail.com>
2026-04-19 05:29:45 -0700

    update dashboard controller

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -3,16 +3,27 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Throwable;
 
 class DashboardController extends Controller
 {
     public function index()
     {
-        $props = [
-            'totalLegacyItems' => \App\Models\CebuLegacy::count(),
-            'totalMessages' => \App\Models\Message::count(),
-            'totalReviews' => \App\Models\Review::count(),
-        ];
+        try {
+            $props = [
+                'totalLegacyItems' => \App\Models\CebuLegacy::count(),
+                'totalMessages' => \App\Models\Message::count(),
+                'totalReviews' => \App\Models\Review::count(),
+            ];
+        } catch (Throwable $e) {
+            report($e);
+
+            $props = [
+                'totalLegacyItems' => 0,
+                'totalMessages' => 0,
+                'totalReviews' => 0,
+            ];
+        }
 
         return inertia('Dashboard', $props);
     }
